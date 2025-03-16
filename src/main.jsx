935bada0e87eb02dbc0f18e70298dc060f6e9b75
Mir Abbas Hussain <141949223+Mir-pes@users.noreply.github.com>
2025-03-16 21:18:30 +0530

    Update main.jsx with Routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter as Router} from "react-router-dom";
+import {BrowserRouter, BrowserRouter as Router, RouterProvider} from "react-router-dom";
+
+
+
+
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
-    <App />
-    </Router>
+    <BrowserRouter>
+    <App/>
+    </BrowserRouter>
   </StrictMode>,
 )
