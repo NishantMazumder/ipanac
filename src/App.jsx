95f9a733b9eca128754184a3bed4808c40253a15
Mir Abbas Hussain <141949223+Mir-pes@users.noreply.github.com>
2025-03-16 21:17:50 +0530

    Update App.jsx with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './navbar.jsx';
-// import Hero from './hero.jsx';
-import FeatureSection from './featuresection.jsx'
+import FeatureSection from './featuresection.jsx';
 import Workflow from './WhyChoose.jsx';
 import Head from './head.jsx';
 import Footer from './footer.jsx';
-function App() {
+import About from './about.jsx';
 
+function App() {
   return (
     <>
-      <Navbar/>
-      <div className='max-w-7xl mx-auto pt-20 x-6'>
-        <Head/>
-        <Workflow/>
-        <FeatureSection/>
-        <Footer/>
-        
-      </div>
-
-    
+      <Navbar />
+      <Routes>
+        <Route path="/" element={
+          <div className='max-w-7xl mx-auto pt-20 x-6'>
+            <Head />
+            <Workflow />
+            <FeatureSection />
+            <Footer />
+          </div>
+        } />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
