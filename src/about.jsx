b1fa31f135aaa58f0d7722124138271565cedcf6
Mir Abbas Hussain <141949223+Mir-pes@users.noreply.github.com>
2025-03-16 21:21:24 +0530

    Add files via upload
    Updated about.jsx and about.css

---
+++ src/about.jsx
@@ -0,0 +1,11 @@
+import "./about.css";
+
+function About(){
+    return(
+        <div className="info">
+        <h1 className="About">What is IPANAC</h1>
+        </div>
+    );
+}
+
+export default About;
